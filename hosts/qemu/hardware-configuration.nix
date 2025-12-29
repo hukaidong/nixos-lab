1faad57e7011fc09e44352854c2f80c38441a895
Kaidong Hu <hukaidonghkd@gmail.com>
2025-12-28 20:45:25 -0500

    feat(qemu): Add qemu option use libvirt to connect with host

--- hosts/qemu/hardware-configuration.nix
+++ hosts/qemu/hardware-configuration.nix
@@ -3,4 +3,12 @@
 
 {
   nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
+
+  # Use bridged networking via virbr0 for direct network access
+  # Requires host to have virtualisation.libvirtd.enable = true
+  # Note: helper= points to SUID wrapper, not QEMU's bundled non-SUID helper
+  virtualisation.qemu.options = [
+    "-netdev bridge,id=net0,br=virbr0,helper=/run/wrappers/bin/qemu-bridge-helper"
+    "-device virtio-net-pci,netdev=net0"
+  ];
 }
