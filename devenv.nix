41a1f86a0c86a684f23d6cf01cdeafd28dfee68c
Kaidong Hu <hukaidonghkd@gmail.com>
2026-01-21 22:57:29 -0500

    chore: Update flake.lock

--- devenv.nix
+++ devenv.nix
@@ -19,4 +19,8 @@
   scripts.deploy.exec = ''
     nixos-rebuild switch --flake .#digitalocean --target-host digix --ask-sudo-password
   '';
+
+  scripts.update.exec = ''
+    nix flake update && git commit -am "chore: Update flake.lock" && git push
+  '';
 }
